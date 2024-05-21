5994f7fa6d4c4be35a1ce91d5546da5f64c4ada6
spex2024 <dercolbags@gmail.com>
2024-05-21 16:25:53 +0000

    initial

--- spex-ui/src/App.jsx
+++ spex-ui/src/App.jsx
@@ -1,23 +1,17 @@
 // src/App.js
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from "./pages/Dashboard.jsx";
 import Register from "./pages/Register.jsx";
-import { UserContext } from "./context/User.jsx";
 
 function App() {
-    const { userData } = useContext(UserContext);
 
-    const ProtectedRoute = ({ element }) => {
-        return userData ? element : <Navigate to="/" replace />;
-    };
 
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+                <Route path="/dashboard"  element={<Dashboard />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
