046f8f293f0ef850ef61b4b41e23ff86b9f3877e
spex2024 <dercolbags@gmail.com>
2024-05-21 16:35:49 +0000

    initial

--- spex-ui/src/component/Register.jsx
+++ spex-ui/src/component/Register.jsx
@@ -27,7 +27,7 @@ function Register() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/register', {
+            const response = await fetch('https://spex.onrender.com/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
