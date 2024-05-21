142b4832282cde77cbd27420f393bd34ca28602e
spex2024 <dercolbags@gmail.com>
2024-05-21 15:54:28 +0000

    initial

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 const app = express();
 app.use(cors({
-    origin: 'https://spex.onrender.com,
+    origin: 'https://spex.onrender.com',
     credentials :true
 }));
 app.use(express.json())
